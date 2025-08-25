f2cb60dff5bde2773f743dbd8c4ed8b427fd89e9
Croc-ker <wyatt.crockerm@gmail.com>
2025-08-25 03:04:43 -0600

    woops

--- Assets/Scripts/Attacks/EnemyNormal.cs
+++ Assets/Scripts/Attacks/EnemyNormal.cs
@@ -6,7 +6,6 @@ public class EnemyNormal : IAttack
     public override float knockback => 8f;
     public override string ignoreTag => "Enemy";
     public override float destroyTime => 0.1f;
-{
     void Update()
     {
         
