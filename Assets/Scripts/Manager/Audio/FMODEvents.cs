effc5577bd0e7ec20560c49d6e09bb033a7f2ca2
Elijah Kelting <elijahakbusiness@gmail.com>
2025-08-27 00:29:23 -0600

    Create Prefab, add more fields

--- Assets/Scripts/Manager/Audio/FMODEvents.cs
+++ Assets/Scripts/Manager/Audio/FMODEvents.cs
@@ -5,6 +5,13 @@ public class FMODEvents : MonoBehaviour
 {
     [field: Header("Knight SFX")]
     [field: SerializeField] public EventReference knightSwordSwing { get; private set; }
+    [field: SerializeField] public EventReference knightWalk { get; private set; }
+    [field: SerializeField] public EventReference knightBlock { get; private set; }
+
+    [field: Header("Wizard SFX")]
+    [field: SerializeField] public EventReference wizardPrimary { get; private set; }
+    [field: SerializeField] public EventReference wizardWalk { get; private set; }
+    [field: SerializeField] public EventReference wizardSecondary { get; private set; }
 
     [field: Header("Menu SFX")]
     [field: SerializeField] public EventReference UIClick {  get; private set; }
