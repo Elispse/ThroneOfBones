ee83d0880c50cd33d732cf26a13e0692257f23c8
Elijah Kelting <elijahakbusiness@gmail.com>
2025-09-02 14:40:49 -0600

    added wizard and UI SFX

--- Assets/Scripts/Manager/Audio/FMODEvents.cs
+++ Assets/Scripts/Manager/Audio/FMODEvents.cs
@@ -6,6 +6,7 @@ public class FMODEvents : MonoBehaviour
     [field: Header("Knight SFX")]
     [field: SerializeField] public EventReference knightSwordSwing { get; private set; }
     [field: SerializeField] public EventReference knightWalk { get; private set; }
+    [field: SerializeField] public EventReference knightShield { get; private set; }
     [field: SerializeField] public EventReference knightBlock { get; private set; }
 
     [field: Header("Wizard SFX")]
@@ -13,14 +14,22 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference wizardWalk { get; private set; }
     [field: SerializeField] public EventReference wizardSecondary { get; private set; }
 
+    [field: Header("Skeleton Enemy SFX")]
+    [field: SerializeField] public EventReference skeletonAttack { get; private set; }
+    [field: SerializeField] public EventReference skeletonDeath { get; private set; }
+    [field: SerializeField] public EventReference skeletonHit { get; private set; }
+
     [field: Header("Menu SFX")]
     [field: SerializeField] public EventReference UIClick {  get; private set; }
 
     [field: Header("Ambience")]
-    [field: SerializeField] public EventReference ambience { get; private set; }
+    [field: SerializeField] public EventReference forestAmbience { get; private set; }
+    [field: SerializeField] public EventReference castleAmbience { get; private set; }
 
     [field: Header("Music")]
-    [field: SerializeField] public EventReference primaryTrack { get; private set; }
+    [field: SerializeField] public EventReference forestMusic { get; private set; }
+    [field: SerializeField] public EventReference castleMusic { get; private set; }
+    [field: SerializeField] public EventReference menuMusic { get; private set; }
     public static FMODEvents instance { get; private set; }
 
     private void Awake()
