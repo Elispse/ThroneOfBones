f79596b569dd8a1a2842452d5ca195b0a98d70e7
Croc-ker <123499932+Croc-ker@users.noreply.github.com>
2025-09-02 18:50:49 -0600

    starting level 1

---
+++ Assets/Scripts/Manager/CameraBound.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraBound : MonoBehaviour
+{
+    [SerializeField] CameraManager cameraManager;
+    void Start()
+    {
+        if (cameraManager == null)
+        {
+            cameraManager = Camera.main.GetComponent<CameraManager>();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cameraManager.NextSection();
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/CameraManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Transform[] sections;
+    private int currentSection = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextSection()
+    {
+        if (currentSection < sections.Length - 1)
+        {
+            currentSection++;
+        }
+        gameObject.transform.position = new Vector3(sections[currentSection].position.x, sections[currentSection].position.y, gameObject.transform.position.z);
+    }
+}
