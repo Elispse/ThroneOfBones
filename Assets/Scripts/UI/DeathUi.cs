ebe993aedb05bbbdcee526ddcb3b0b091df07b74
Michael <114317916+MFunken7@users.noreply.github.com>
2025-09-03 04:00:43 -0600

    script made

---
+++ Assets/Scripts/UI/DeathUi.cs
@@ -0,0 +1,24 @@
+using FMOD.Studio;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DeathUi : MonoBehaviour
+{
+	private EventInstance buttonsfx;
+
+
+	private UIDocument uiDocument;
+
+	private Button respawnButton;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
