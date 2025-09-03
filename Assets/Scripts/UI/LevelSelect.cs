86c550fe4e82e6e3fec9c2c9bcb572a2f87c2659
Michael <114317916+MFunken7@users.noreply.github.com>
2025-09-02 23:24:13 -0600

    Audio Settings set up

--- Assets/Scripts/UI/LevelSelect.cs
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -6,25 +7,54 @@ public class LevelSelect : MonoBehaviour
 {
 
     private UIDocument uiDocument;
-    private Button leveltestButton;
     private Button level1Button;
-    private Button fivehundredwolvesButton;
+    private Button CloseButton;
+    private Button SettingsButton;
+    private Button SettingsCloseButton;
+    private VisualElement settings;
+
+    private bool settingsOpen = false;
+
     void Start()
     {
         uiDocument = GetComponent<UIDocument>();
-        leveltestButton = uiDocument.rootVisualElement.Q<Button>("btnlvltest");
         level1Button = uiDocument.rootVisualElement.Q<Button>("btnlvl1");
-        fivehundredwolvesButton = uiDocument.rootVisualElement.Q<Button>("btnlvlwolf");
+        CloseButton = uiDocument.rootVisualElement.Q<Button>("btnclose");
+        settings = uiDocument.rootVisualElement.Q<VisualElement>("Settings");
+        SettingsButton = uiDocument.rootVisualElement.Q<Button>("btnsettings");
+        SettingsCloseButton = uiDocument.rootVisualElement.Q<Button>("btnsettingsclose");
 
-        leveltestButton.clicked += () => GoToLevel(2);
         level1Button.clicked += () => GoToLevel(3);
-        fivehundredwolvesButton.clicked += () => GoToLevel(4);
+        CloseButton.clicked += () => Close();
+        SettingsButton.clicked += () => Settings();
+        SettingsCloseButton.clicked += () => Settings();
 
+
+        settings.style.visibility = Visibility.Hidden;
     }
     public void GoToLevel(int index)
     {
         GameManager.Instance.selectedLevel = index;
         //Debug.Log("Loading level: " + name);
         GameManager.Instance.GoToLevel(1);
+    }
+
+    public void Close()
+    {
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+        if (settingsOpen)
+        {
+            settings.style.visibility = Visibility.Hidden;
+            settingsOpen = false;
+        }
+        else
+        {
+            settings.style.visibility = Visibility.Visible;
+            settingsOpen = true;
+        }
     }
 }
